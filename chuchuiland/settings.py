5eaa5d5e7192608644fae3899a6e4a3c283989b7
nyoknyok <kimseonho1202@gmail.com>
2018-07-30 19:25:34 +0900

    Hackerthon Project

--- chuchuiland/settings.py
+++ chuchuiland/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$1o!f5&#30(sq!ak-j7i*a7+-54id_fe2domc^h&d&i0cb$s6$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'smartkuma.iptime.org','192.168.0.3']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.smartkuma.iptime.org','192.168.0.3']
 
 
 # Application definition
--- htmltest/views.py
+++ htmltest/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+	return render(request, 'htmltest/index.html', {})
+
+def newsfeed(request):
+	return render(request, 'htmltest/newsfeed.html', {})
+
+def chatroom(request):
+    return render(request, 'htmltest/chatroom.html', {})
+
